263d35d64ec81f51a246ce0bace66b47748498f3
barraotieno <barrackotieno160@gmail.com>
2025-06-15 23:18:49 +0300

    feat: add stock struct in engine.go file
    This struct will hold the available items and their quantities.

--- engine/engine.go
+++ engine/engine.go
@@ -9,3 +9,8 @@ type Engine struct {
 	Cycle           int
 	OptimizeTargets []string
 }
+
+// Stock represents the available items and their quantities.
+type Stock struct {
+	Items map[string]int
+}
