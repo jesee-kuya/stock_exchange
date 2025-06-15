13b225ee8d7cf076fb117fb6352b8d33833e7543
skisenge <skisengese@gmail.com>
2025-06-16 00:16:04 +0300

    feat: initial commit on LoadConfig() method

---
+++ checker/load_config.go
@@ -0,0 +1,7 @@
+package checker
+
+// LoadConfig parses a configuration file and populates the Checker with
+// initial stocks, processes, and optimization goals.
+func (c *Checker) LoadConfig(path string) error {
+	return nil
+}
