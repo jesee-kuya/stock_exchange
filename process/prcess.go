9691fd6c35c18c2e22df6869292fea7a4abf4c72
JoeOwino <Joeoowino@gmail.com>
2025-06-15 21:29:54 +0300

    chore: update comments for Process struct to enhance clarity

--- process/prcess.go
+++ process/prcess.go
@@ -1,5 +1,8 @@
 package process
 
+// Process represents a processing unit in the stock exchange system.
+// It defines the name of the process, the required input resources (Needs),
+// the output resources produced (Result), and the number of cycles needed to complete the process.
 type Process struct {
 	Name         string
 	Needs        map[string]int
