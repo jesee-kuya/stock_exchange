39780e754b148042bd1a40c48c3f09888daefbf2
skisenge <skisengese@gmail.com>
2025-06-16 15:01:01 +0300

    test: add parsestock test

---
+++ util/parse_test.go
@@ -0,0 +1,36 @@
+package util
+
+import "testing"
+
+func TestParseLine(t *testing.T) {
+	// configData := &ConfigData{}
+	testCases := []struct {
+		name    string
+		rawData string
+	}{
+		{"parse stock", "cabinet:1"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			configData := &ConfigData{
+				Stocks: map[string]int{},
+			}
+			err := parseLine(configData, tc.rawData)
+			if err != nil {
+				t.Errorf("%v", err)
+			}
+		})
+	}
+	t.Run("parse stock", func(t *testing.T) {
+		ConfigData := &ConfigData{
+			Stocks: map[string]int{},
+		}
+		rawData := "cabinet:1"
+
+		err := parseLine(ConfigData, rawData)
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+	})
+}
