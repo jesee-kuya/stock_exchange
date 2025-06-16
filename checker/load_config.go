2babbe550b96f92f0ca1968d0a2804123da2440b
skisenge <skisengese@gmail.com>
2025-06-16 06:43:51 +0300

    feat: add file openning feature

--- checker/load_config.go
+++ checker/load_config.go
@@ -1,7 +1,17 @@
 package checker
 
+import (
+	"fmt"
+	"os"
+)
+
 // LoadConfig parses a configuration file and populates the Checker with
 // initial stocks, processes, and optimization goals.
 func (c *Checker) LoadConfig(path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("failed to open config file: %w", err)
+	}
+	defer file.Close()
 	return nil
 }
