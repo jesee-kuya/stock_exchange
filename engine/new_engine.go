964336bed68efb77617cd6ae9c45a52f744a8f7e
JoeOwino <Joeoowino@gmail.com>
2025-06-17 23:12:54 +0300

    docs: enhance comments for NewEngine function to improve clarity and detail

--- engine/new_engine.go
+++ engine/new_engine.go
@@ -2,7 +2,12 @@ package engine
 
 import "github.com/jesee-kuya/stock_exchange/process"
 
-// NewEngine creates and initializes a new Engine instance.
+// NewEngine creates and returns a pointer to a new Engine instance with initialized fields.
+// The Stock field is initialized with an empty map of items.
+// The Processes field is initialized as an empty slice of *process.Process.
+// The Schedule field is initialized as an empty slice of strings.
+// The Cycle field is set to 0.
+// The OptimizeTargets field is initialized as an empty slice of strings.
 func NewEngine() *Engine {
 	return &Engine{
 		Stock:           &Stock{Items: make(map[string]int)},
