6a5e044019d4d58635dc38f7a137f5ff8ef18c55
skisenge <skisengese@gmail.com>
2025-06-19 18:15:46 +0300

    chore: refactor error message

--- engine/load_config.go
+++ engine/load_config.go
@@ -1,15 +1,13 @@
 package engine
 
 import (
-	"fmt"
-
 	"github.com/jesee-kuya/stock_exchange/util"
 )
 
 func (e *Engine) LoadConfig(path string) error {
 	config, err := util.ParseConfig(path)
 	if err != nil {
-		return fmt.Errorf("failed to parse config: %w", err)
+		return err
 	}
 
 	e.Stock = &Stock{Items: config.Stocks}
